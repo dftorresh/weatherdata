48ac07b01244a1ef8993d098292c20ef70258e44
Daniel Torres <daniel_torres@epam.com>
2025-03-07 16:27:31 -0500

    Added error handling to the extract script. Created function to read cities json file in the transform script

--- scripts/extract.py
+++ scripts/extract.py
@@ -6,26 +6,42 @@ from datetime import datetime
 def fetch_weather_data(appid, lat, lon, units, exclude=None):
     url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={appid}&units={units}"
     url += f"&exclude={exclude}" if exclude else ""
-    api_response = requests.get(url=url)
-    api_response.raise_for_status
-    if api_response.status_code != 200:
-        raise Exception(f"Error when calling weather API. Error code {api_response.status_code}")
-    return api_response.json()
+    try:
+        api_response = requests.get(url=url)
+        api_response.raise_for_status()
+        return api_response.json()
+    except requests.exceptions.HTTPError as e:
+        custom_message = f"Weather API returned an error: {str(e)}"
+        e.args = (custom_message,)
+        raise
+    except requests.exceptions.RequestException as e2:
+        custom_message2 = f"Error calling weather API: {str(e2)}"
+        e2.args = (custom_message2,)
+        raise
 
 
 def upload_data_to_bucket(weather_data, bucket_name, folder):
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
+    try:
+        storage_client = storage.Client()
+        bucket = storage_client.get_bucket(bucket_name)
+    except Exception as e:
+        custom_message = f"Error accessing GCP bucket: {str(e)}"
+        e.args = (custom_message,)
+        raise
     current_date = datetime.now()
     formated_current_date = current_date.strftime("%Y%m%d_%H%M%S")
     json_file_name = f"{folder}/weather_data_{formated_current_date}.json"
-    blob = bucket.blob(json_file_name)
-    blob.upload_from_string(data=str(weather_data))
-    return json_file_name
+    try:
+        blob = bucket.blob(json_file_name)
+        blob.upload_from_string(data=str(weather_data))
+        return json_file_name
+    except Exception as e2:
+        custom_message2 = f"Error writing json file to GCP bucket: {str(e2)}"
+        e2.args = (custom_message2,)
+        raise
 
 
 def extract_weather_data_into_bucket(appid, lat, lon,  units, exclude, bucket_name, folder):
     weather_data = fetch_weather_data(appid, lat, lon,  units, exclude)
     json_file_name = upload_data_to_bucket(weather_data, bucket_name, folder)
     return json_file_name
-
--- scripts/transform.py
+++ scripts/transform.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from google.cloud import storage
+import json
 
 def transform(**kwargs): #bucket_name, json_data_file
     task_instance = kwargs['ti']
@@ -8,4 +9,14 @@ def transform(**kwargs): #bucket_name, json_data_file
     bucket = storage_client.get_bucket(kwargs["bucket_name"])
     blob = bucket.blob(json_data_file)
     json_data = blob.download_as_text()
+    read_cities_file()
     return json_data
+
+
+def read_cities_file():
+    storage_client1 = storage.Client()
+    bucket1 = storage_client1.get_bucket("airqualityproject28")
+    blob1 = bucket1.blob("data/cities.json")
+    json_cities = blob1.download_as_text()
+    cities_dict = json.loads(json_cities)
+    print(cities_dict)
